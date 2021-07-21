af1de166dd4fd0b5e0abc9c7e97e33a4f2838ae8
DIAFAT <Lyamin.diafat@sfr.fr>
2021-07-21 16:35:19 +0200

    4 et 5 fini

--- 4-destructuring/script.js
+++ 4-destructuring/script.js
@@ -1,13 +1,17 @@
 //4.1 Découverte
 
-const [a, b] = [1, 2, 3, 4]
+const [a, b, c] = [1, 2, 3, 4]
 console.log(a)
 console.log(b)
-
-const {first, last} = {first: 'Paul', last: 'Henta', age: 35}
+console.log(c)
+//a est la 1ere valeur[0] du tableau et , la 2eme valeur [1]
+const {first, last, age} = {first: 'Paul', last: 'Henta', age: 35}
 console.log(first)
 console.log(last)
+console.log(age)
 
 //4.2 Application
-
-console.log(data)
+ Object.values(data).forEach(function(element){
+    const {texte, dateDebut, dateFin} = element;
+    console.log(texte, dateDebut, dateFin)
+})
--- 5-import/script.js
+++ 5-import/script.js
@@ -1,8 +1,14 @@
 //5-1 Import
 
-import {hello} from './functions.js'
-
+import {hello} from './functions.js';
 hello()
 
+import {showError} from './functions.js';
+showError('erreur')
+
+import anonyme from './functions.js';
+anonyme()
+
 // 5-2 Export
-const [a, b] = [7, 9]
+const [a, b] = [7, 9] 
+export * from './math.js';
