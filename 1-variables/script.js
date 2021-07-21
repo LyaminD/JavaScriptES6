fa6ae6ffb779813336f2c0a07011f9084c7d570d
DIAFAT <Lyamin.diafat@sfr.fr>
2021-07-21 09:51:33 +0200

    exo1fini

--- 1-variables/script.js
+++ 1-variables/script.js
@@ -1,11 +1,11 @@
 //1.1 Types simples
 
-var name = 'Jacques'
-var age = 80
-var useless = 'something'
+const name = 'Jacques'
+let age = 80
+let useless = 'something'
 
 console.log(name, age, useless)
-
+//variable déclarée avec const peut pas etre redéclarée
 age += 1
 useless = 14
 
@@ -14,20 +14,20 @@ console.log(name, age, useless)
 //----------------------//
 //1.2 Tableaux
 
-let fruits = ['apple', 'pear', 'cherry']
-let vegetables = ['potato', 'curlyflower', 'tomato']
+const fruits = ['apple', 'pear', 'cherry']
+const vegetables = ['potato', 'curlyflower', 'tomato']
 
 console.log(fruits, vegetables)
 
 fruits.push('banana')
 vegetables.pop()
-
+//on push banana dans le 1er tableau et en enleve tomato dans le 2e tableau
 console.log(fruits, vegetables)
 
 //----------------------//
 //1.2 Objets
 
-let settings = {
+const settings = {
     sound: true,
     music: false,
     graphics: 'high',
@@ -41,7 +41,7 @@ let savedGame = {
 }
 
 console.log(settings, savedGame)
-
+//Une const est une valeur qui ne peut pas être modifiée. Elle ne peut être modifiée avec une réaffectation ou une redéclaration.
 savedGame = {}
 settings.music = true
 settings.resolution = [3840, 2160]
